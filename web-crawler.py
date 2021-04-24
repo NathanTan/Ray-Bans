dacb8398e97265ff72b5fe2ac9cf4e87188a2b66
Nathan Tan <nathantan97@gmail.com>
2021-04-23 20:40:22 -0700

    Can't run the code on this computer so I guess
    this is a good stopping point.

--- web-crawler.py
+++ web-crawler.py
@@ -12,6 +12,7 @@ class Crawler:
     def __init__(self, urls=[]):
         self.visited_urls = []
         self.urls_to_visit = urls
+        self.url_count = 0
 
     def download_url(self, url):
         return requests.get(url).text
@@ -20,19 +21,19 @@ class Crawler:
         soup = BeautifulSoup(html, 'html.parser')
         for link in soup.find_all('a'):
             #TODO: check if the link tag has the rb-plp-product-tile value in the attribute class
-            #if link.
-            path = link.get('href')
-            if path and path.startswith('/'):
-                path = urljoin(url, path)
-            yield path
+            if 'rb-plp-product-tile' in link.get('class'):
+                path = link.get('href')
+                if path and path.startswith('/'):
+                    path = urljoin(url, path)
+                yield path
 
     def add_url_to_visit(self, url):
-        if 
         if url not in self.visited_urls and url not in self.urls_to_visit:
             self.urls_to_visit.append(url)
 
     # Do the actual crawling of the webpage
     def crawl(self, url):
+        self.url_count += 1
         html = self.download_url(url)
         for url in self.get_linked_urls(url, html):
             
