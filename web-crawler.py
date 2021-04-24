59106c754e6a0756770188627388eeccc439e703
NathanTan <tann@oregonstate.edu>
2021-04-24 11:36:33 -0700

    Having issues where url gets encoded before sendin

--- web-crawler.py
+++ web-crawler.py
@@ -15,11 +15,15 @@ class Crawler:
         self.url_count = 0
 
     def download_url(self, url):
-        return requests.get(url).text
+        print(f'Getting for url: {url}')
+        r = requests.get(str(url))
+        print(f'response: {r}')
+        return r.text
 
     def get_linked_urls(self, url, html):
         soup = BeautifulSoup(html, 'html.parser')
         for link in soup.find_all('a'):
+            print(f'Link: {link}')
             #TODO: check if the link tag has the rb-plp-product-tile value in the attribute class
             if 'rb-plp-product-tile' in link.get('class'):
                 path = link.get('href')
@@ -35,8 +39,10 @@ class Crawler:
     def crawl(self, url):
         self.url_count += 1
         html = self.download_url(url)
+        print(html)
+        if 'You don\'t have permission' in html:
+            raise Exception('Access denied') 
         for url in self.get_linked_urls(url, html):
-            
             self.add_url_to_visit(url)
 
     def run(self):
@@ -51,4 +57,6 @@ class Crawler:
                 self.visited_urls.append(url)
 
 if __name__ == '__main__':
-    Crawler(urls=['https://www.ray-ban.com/usa/sunglasses/view-all']).run()
+    c = Crawler(urls=['https://www.ray-ban.com/usa/sunglasses/view-all'])
+    c.run()
+    print(c.url_count)
